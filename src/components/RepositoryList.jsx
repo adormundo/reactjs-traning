90841c452ee6eeecc7ed4b03969b2c6ca4a27552
Alvaro Dormundo <adormundo@gmail.com>
2022-12-15 23:33:57 -0300

    component item and props

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,31 +1,14 @@
+import { RepositoryItem } from './RepositoryItem';
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar respositório</a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar respositório</a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar respositório</a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar respositório</a>
-        </li>
+        <RepositoryItem repository="unform2" />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
       </ul>
     </section>
   );
